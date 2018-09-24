c51e1e0bd85183f204683c1a861a74c81d5641d1
Chintogtokh Batbold <bchintogtokh@gmail.com>
2018-09-24 15:00:09 +1000

    Changes made to support PHP7

--- lib/Doctrine/Query/Abstract.php
+++ lib/Doctrine/Query/Abstract.php
@@ -1118,8 +1118,9 @@ abstract class Doctrine_Query_Abstract
                 $params = array('component' => $component, 'alias' => $alias);
                 $event = new Doctrine_Event($record, $callback['const'], $this, $params);
 
-                $record->$callback['callback']($event);
-                $table->getRecordListener()->$callback['callback']($event);
+                // changed for php7 support
+                $record->{$callback['callback']}($event);
+                $table->getRecordListener()->{$callback['callback']}($event);
             }
         }
 
